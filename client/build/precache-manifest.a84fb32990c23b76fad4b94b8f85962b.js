07df06dd9f6dcc1e86cd083e29a795bbdebcff08
Austin Graves <austingraves106@aol.com>
2019-12-21 09:54:39 -0600

    I am gonna try netlify. So I created the build folder

---
+++ client/build/precache-manifest.a84fb32990c23b76fad4b94b8f85962b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd905e293f8e297653a6248fe4ae1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "029f0c9a1e08b74d4cb1",
+    "url": "/static/css/main.a1c1b669.chunk.css"
+  },
+  {
+    "revision": "a6381bed3f4d2cc675e8",
+    "url": "/static/js/2.8e8e611c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8e8e611c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "029f0c9a1e08b74d4cb1",
+    "url": "/static/js/main.93173dea.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
